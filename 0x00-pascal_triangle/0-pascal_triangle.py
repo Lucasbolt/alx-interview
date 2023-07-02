13a9f6a1575c22d58bb34e5cd590c2ac1a3b972b
Chisom Ibekwe <lucasbolt700@gmail.com>
2023-07-02 20:05:17 +0100

    pascal triangle task

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+def pascal_triangle(n):
+    """
+    this function generates list of pascal triangle of the input n.
+    """
+    Result = []
+    if n == 0:
+        return Result
+    for i in range(1, n+1):
+        row = [1]
+        if len(row) == i:
+            Result.append(row)
+        else:
+            prev_row = Result[-1]
+            for a in range(1, i+1):
+                try:
+                    new = prev_row[a-1] + prev_row[a]
+                    row.append(new)
+                except IndexError:
+                    new = prev_row[a-1] + 0
+                    row.append(new)
+                    Result.append(row)
+                    break
+    return Result
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,1 @@
+This directory contains a script to generate pascal triangles as a list.
