cc45e4650760a0c1baf8f493aa5491fb02ba9ba9
Chisom Ibekwe <lucasbolt700@gmail.com>
2023-07-17 03:48:51 +0100

    task

---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,1 @@
+this directory contains the solution to the alx-interview problem 'minimum operations'.
---
+++ 0x02-minimum_operations/test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+minOperations = __import__('0-minoperations').minOperations
+
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 9
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+n = 20
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 8
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 13
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 95
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 10
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 100
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
