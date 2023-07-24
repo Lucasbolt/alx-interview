008f48dafd7b3fc2464d1d0cf8d47b6f32365849
Chisom Ibekwe <lucasbolt700@gmail.com>
2023-07-24 05:33:54 +0100

    task

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""log parsing module."""
+import sys
+
+
+while True:
+    nl = 0
+    fsize = 0
+    ld = {}
+    try:
+        while nl < 10:
+            line = sys.stdin.readline()
+            nl += 1
+            if 'GET /projects' in line:
+                fsize += int(line.split()[-1])
+                stat = line.split()[-2]
+                try:
+                    stat = int(stat)
+                    if stat in ld.keys():
+                        ld[stat] += 1
+                    else:
+                        ld[stat] = 1
+                except ValueError:
+                    pass
+        print(f'File size: {fsize}')
+        for a in sorted(ld):
+            print(f'{a}: {ld[a]}')
+    except KeyboardInterrupt:
+        break
