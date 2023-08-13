9539cc933eeb851e55bcb5ee301f65e45bc96795
Chisom Ibekwe <lucasbolt700@gmail.com>
2023-08-13 23:36:41 +0100

    fixed code

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,23 +3,23 @@ const request = require('request');
 const API_URL = 'https://swapi-api.hbtn.io/api';
 
 if (process.argv.length > 2) {
-  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-    if (err) {
-      console.log(err);
+  request(`${API_URL}/films/${process.argv[2]}/`, (error, response, responseBody) => {
+    if (error) {
+      console.log(error);
     }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersName = charactersURL.map(
+    const charactersURLs = JSON.parse(responseBody).characters;
+    const charactersNamesPromises = charactersURLs.map(
       url => new Promise((resolve, reject) => {
-        request(url, (promiseErr, __, charactersReqBody) => {
-          if (promiseErr) {
-            reject(promiseErr);
+        request(url, (promiseError, promiseResponse, charactersBody) => {
+          if (promiseError) {
+            reject(promiseError);
           }
-          resolve(JSON.parse(charactersReqBody).name);
+          resolve(JSON.parse(charactersBody).name);
         });
       }));
 
-    Promise.all(charactersName)
+    Promise.all(charactersNamesPromises)
       .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
+      .catch(allErrors => console.log(allErrors));
   });
 }
