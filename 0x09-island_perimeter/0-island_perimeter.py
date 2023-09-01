c13cd84c55df890cca45d3d956d3f3251d9461e4
Chisom Ibekwe <lucasbolt700@gmail.com>
2023-09-01 03:49:34 +0100

    task

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module defines the island_perimeter function.
+"""
+
+
+def island_perimeter(grid):
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4  # Initialize perimeter for land cell
+
+                # Check left neighbor
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+                # Check upper neighbor
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+
+    return perimeter
---
+++ 0x09-island_perimeter/README.md
@@ -0,0 +1,1 @@
+This directory contains a python script to calculate the perimeter of an island.
